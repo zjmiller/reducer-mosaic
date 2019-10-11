e92df402a850e56cc2098f978326e43fcd4ee886
William Saunders <william@williamsaunders.net>
2019-10-10 17:36:12 -0700

    Change scheduler to deprioritize question generation workspaces

--- server/src/script-registry/estimation-script/scheduling.ts
+++ server/src/script-registry/estimation-script/scheduling.ts
@@ -1,12 +1,22 @@
-import { IState } from "./types";
-import { getAllWorkspaces } from "./utils";
+import { User } from '../../user';
+import { IState } from './types';
+import { getAllWorkspaces } from './utils';
 
-import { User } from "../../user";
+
+function workspacePriority(workspace: any): number {
+  switch (workspace.workspaceType) {
+    case "DECOMPOSITION_WORKSPACE": return 0;
+    case "FORMALIZE_QUESTION_WORKSPACE": return 1;
+    case "GENERATE_QUESTIONS_WORKSPACE": return 2;
+  }
+  return 999;
+}
 
 export function getEligibleInteractionsForUser(state: IState, user: User) {
-  const allWorkspaces = getAllWorkspaces(state);
-
-  return allWorkspaces.filter(w => w.isActive && w.assignedTo === null);
+  let allWorkspaces = getAllWorkspaces(state);
+  allWorkspaces = allWorkspaces.filter(w => w.isActive && w.assignedTo === null);
+  allWorkspaces = allWorkspaces.sort((a, b) => (workspacePriority(a) - workspacePriority(b)));
+  return allWorkspaces;
 }
 
 export function getAlreadyAssignedInteractionsForUser(
